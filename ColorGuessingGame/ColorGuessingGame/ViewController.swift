4053d3419b5a9331302ee2038dab2d3ad033e413
Margiett Gil <mxg627@icloud.com>
2019-10-29 18:08:14 -0400

    getting started

---
+++ ColorGuessingGame/ColorGuessingGame/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  ColorGuessingGame
+//
+//  Created by Margiett Gil on 10/29/19.
+//  Copyright © 2019 Margiett Gil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var red: UIButton!
+    
+    @IBOutlet weak var green: UIButton!
+    
+    @IBOutlet weak var blue: UIButton!
+    
+    @IBOutlet weak var generatedColorView: UIView!
+    
+    override func viewDidLoad() {
+         super.viewDidLoad()
+         // Do any additional setup after loading the view.
+     }
+    
+    @IBAction func newGame(_ sender: UIButton) {
+//        let randomColor = Int.random(in: 0...2)
+//        switch sender.tag {
+//        case randomColor == generatedColorView
+//            print(
+//
+//        default:
+//            print("default")
+//        }
+//        let randomColor.tag
+//        let randomColorSelection = randomColor.randomElement()
+//        if randomColorSelection == 0 {
+//            generatedColorView.backgroundColor = .red
+//
+//        }
+//    }
+
+
+}
+
+}
